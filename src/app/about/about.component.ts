1632a8868ccd0fc39ac6c7a308843cd45333e791
veerbhadrasolanki <veerbhadrasingh123@gmail.com>
2025-06-15 14:10:09 +0530

    Add new angular learning project task manager to it.

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  standalone: false,
+  templateUrl: './about.component.html',
+  styleUrl: './about.component.scss'
+})
+export class AboutComponent {
+
+}
